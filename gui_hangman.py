f23ac36d0077a764f3bec4d34e81290c1890fd62
Tech-notYet <30681614+Tech-notYet@users.noreply.github.com>
2020-03-02 11:11:16 -0800

    function initialize_frame correctly unpacks

--- gui_hangman.py
+++ gui_hangman.py
@@ -12,7 +12,7 @@ def startup_app():
     APP.rowconfigure(0, minsize=800, weight=1)
     APP.columnconfigure([0, 1, 2], weight=1)
 
-    left_parent, center_parent, right_parent = initialize_frame(('left_parent', 'center_parent', 'right_parent'), APP)
+    left_parent, center_parent, right_parent = initialize_frame(['left_parent', 'center_parent', 'right_parent'], APP)
     for parent in left_parent, center_parent, right_parent:
         parent.rowconfigure(0, weight=1)
         parent.columnconfigure(0, weight=1)
@@ -22,25 +22,25 @@ def startup_app():
 
 
 
-def initialize_frame(names: typ.Tuple[str], master: typ.Optional[tk.Toplevel] = None) -> typ.Tuple[ttk.Frame]:
+def initialize_frame(names: typ.List[str], master: typ.Optional[tk.Tk] = None) -> typ.Tuple[ttk.Frame]:
     """Take names and master ordering as left-center-right),
     and return a tuple of Frame widgets"""
 
     try:
         # Left Frame containing the canvas widget
-        _left = ttk.Frame(master, class_='ParentFrame', height=800, width=700, padding=15,
+        left = ttk.Frame(master, class_='ParentFrame', height=600, width=800, padding=15,
                           relief=tk.GROOVE, name=names[0], borderwidth=1)
         # Center Frame containing the Text widget to display previous choices
-        _center = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
+        center = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
                             name=names[1], borderwidth=1)
         # Right Frame containing the canvas widget
-        _right = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
-                           relief=tk.SUNKEN, name=names[3], borderwidth=1)
+        right = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
+                           relief=tk.SUNKEN, name=names[2], borderwidth=1)
     except IndexError:
         print('3 names needed for frames')
         return tuple()
 
-    return tuple(_left, _center, _right)
+    return (left, center, right)
 
 if __name__ == '__main__':
     startup_app()
