525e33e080c07401b4d8b00180df5777e622a7be
Tech-notYet <30681614+Tech-notYet@users.noreply.github.com>
2020-03-04 10:54:36 -0800

    created Layout class to configure app layout on startup

--- gui_hangman.py
+++ gui_hangman.py
@@ -1,95 +1,82 @@
-import tkinter.ttk as ttk
+import typing as Tp
+from tkinter import ttk as ttk
 from tkinter.ttk import tkinter as tk
-from typing import *
+from itertools import starmap
 
-GenericWidget = TypeVar('GenericWidget',
-                        ttk.Button,
-                        ttk.Checkbutton,
-                        ttk.Combobox,
-                        ttk.Entry,
-                        ttk.Frame,
-                        ttk.Label,
-                        ttk.Labelframe,
-                        ttk.LabelFrame,
-                        ttk.Menubutton,
-                        ttk.Notebook,
-                        ttk.Panedwindow,
-                        ttk.PanedWindow,
-                        ttk.Progressbar,
-                        ttk.Radiobutton,
-                        ttk.Scale,
-                        ttk.Scrollbar,
-                        ttk.Separator,
-                        ttk.Sizegrip,
-                        ttk.Spinbox,
-                        ttk.Style,
-                        ttk.Treeview,
-                        ttk.LabeledScale,
-                        ttk.OptionMenu)
-
-WidgetOpts = NewType('WidgetOpts', Mapping[str, Union[str, int]])
-
-def startup_app() -> None:
-    APP_ROWS = [0]
-    APP_COLS = [0, 1, 2]
-
-    # Left and Center Frames consist of 1x1 row, col
-    LP_ROWS = [0]
-    LP_COLS = [0]
-    CP_ROWS = [0]
-    CP_COLS = [0]
-    # Right Frames contains input, buttons, text; it consists of 2x1 row, col
-    RP_ROWS = [0, 1]
-    RP_COLS = [0]
-
-    # Top level window
-    APP = tk.Tk()
-    APP.title('Hangman In Python')
-    APP.rowconfigure(APP_ROWS, weight=1, minsize=400)
-    APP.columnconfigure(APP_COLS, weight=1, minsize=400)
-
-    left_parent, center_parent, right_parent = initialize_frame(['left_parent', 'center_parent', 'right_parent'], APP)
-    for parent in left_parent, center_parent, right_parent:
-        if parent.winfo_name() == 'left_parent':
-            parent.rowconfigure(LP_ROWS, weight=1)
-            parent.columnconfigure(LP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[0])
-        elif parent.winfo_name() == 'center_parent':
-            parent.rowconfigure(CP_ROWS, weight=1)
-            parent.columnconfigure(CP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[1])
-        else:
-            parent.rowconfigure(RP_ROWS, weight=1)
-            parent.columnconfigure(RP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[2])
-
-    APP.mainloop()
+# Top structure classes
 
 
-def grid_widgets(widget: Type[GenericWidget], widget_opts: Mapping[str, Type[Sequence]]) -> None:
-    widget.grid_configure(**widget_opts)
+class LeftFrame(ttk.Frame):
+    """Child of MainApplication. It contains the Canvas widget"""
+
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+        self.configure(width=400, height=400, padding='0.25i', borderwidth=1, relief=tk.RIDGE)
+        self.grid(column=0, row=0, sticky=tk.NSEW, padx=20, pady=20)
 
 
-def initialize_frame(names: List[str], master: Optional[tk.Tk] = None) -> Tuple[ttk.Frame]:
-    """Take names and master ordering as left-center-right),
-    and return a tuple of Frame widgets"""
+class RightFrame(ttk.Frame):
+    """Child of MainApplication. It contains the remaining widgets"""
 
-    try:
-        # Left Frame containing the canvas widget
-        left = ttk.Frame(master, class_='ParentFrame', height=600, width=500, padding=15,
-                         relief=tk.GROOVE, name=names[0], borderwidth=1)
-        # Center Frame containing the Text widget to display previous choices
-        center = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
-                           name=names[1], borderwidth=1)
-        # Right Frame containing the canvas widget
-        right = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
-                          relief=tk.SUNKEN, name=names[2], borderwidth=1)
-    except IndexError:
-        print('3 names needed for frames')
-        return tuple()
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+        self.configure(width=400, height=400, padding='0.25i', borderwidth=1, relief=tk.GROOVE)
+        # 1x3 Grid to fit widgets
+        self.rowconfigure([0, 1, 2], weight=1)
+        self.columnconfigure(0, weight=1)
+        # Grid into column 2 of MainApp
+        self.grid(column=1, row=0, sticky=tk.NSEW, padx=20, pady=20)
 
-    return (left, center, right)
+
+class HangmanCanvas(tk.Canvas):
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+        self.grid(sticky=tk.NSEW)
+        line = []
+        for y in range(5, 125, 5):
+            line.append([5, y])
+        self.create_line(line)
+
+
+class GuessesLabel(ttk.Label):
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+
+
+class GuessEntry(ttk.Entry):
+    def __init__(self, master=None, widget=None, **kw):
+        super().__init__(master=master, widget=widget, **kw)
+
+
+class SubmitButton(ttk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+
+
+class ClearButton(ttk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+
+
+class MainApplication(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        super().__init__(master=master, **kw)
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1, minsize=100)
+        top.columnconfigure(0, weight=1, minsize=100)
+        self.rowconfigure(0, weight=1, minsize=100)
+        self.columnconfigure([0, 1], weight=1, minsize=100)
+        self.grid(sticky=tk.NSEW, padx=25, pady=25)
+
+        self.left = LeftFrame(self)
+        self.right = RightFrame(self)
+        self.hangmanCanvas = HangmanCanvas(self.left)
+        self.guessesLabel = GuessesLabel(self.right)
+        self.guessEntry = GuessEntry(self.right)
+        self.submitButton = SubmitButton(self.right)
+        self.clearButton = ClearButton(self.right)
 
 
 if __name__ == '__main__':
-    startup_app()
+    main = MainApplication()
+    main.mainloop()
