c68dbdd46333baab93f48fa0e519ed4c499592e5
Tech-notYet <30681614+Tech-notYet@users.noreply.github.com>
2020-03-03 18:25:09 -0800

    created Layout class to configure app layout on startup

--- gui_hangman.py
+++ gui_hangman.py
@@ -1,95 +1,51 @@
-import tkinter.ttk as ttk
+import typing as Tp
+from tkinter import ttk as ttk
 from tkinter.ttk import tkinter as tk
-from typing import *
-
-GenericWidget = TypeVar('GenericWidget',
-                        ttk.Button,
-                        ttk.Checkbutton,
-                        ttk.Combobox,
-                        ttk.Entry,
-                        ttk.Frame,
-                        ttk.Label,
-                        ttk.Labelframe,
-                        ttk.LabelFrame,
-                        ttk.Menubutton,
-                        ttk.Notebook,
-                        ttk.Panedwindow,
-                        ttk.PanedWindow,
-                        ttk.Progressbar,
-                        ttk.Radiobutton,
-                        ttk.Scale,
-                        ttk.Scrollbar,
-                        ttk.Separator,
-                        ttk.Sizegrip,
-                        ttk.Spinbox,
-                        ttk.Style,
-                        ttk.Treeview,
-                        ttk.LabeledScale,
-                        ttk.OptionMenu)
-
-WidgetOpts = NewType('WidgetOpts', Mapping[str, Union[str, int]])
-
-def startup_app() -> None:
-    APP_ROWS = [0]
-    APP_COLS = [0, 1, 2]
-
-    # Left and Center Frames consist of 1x1 row, col
-    LP_ROWS = [0]
-    LP_COLS = [0]
-    CP_ROWS = [0]
-    CP_COLS = [0]
-    # Right Frames contains input, buttons, text; it consists of 2x1 row, col
-    RP_ROWS = [0, 1]
-    RP_COLS = [0]
-
-    # Top level window
-    APP = tk.Tk()
-    APP.title('Hangman In Python')
-    APP.rowconfigure(APP_ROWS, weight=1, minsize=400)
-    APP.columnconfigure(APP_COLS, weight=1, minsize=400)
-
-    left_parent, center_parent, right_parent = initialize_frame(['left_parent', 'center_parent', 'right_parent'], APP)
-    for parent in left_parent, center_parent, right_parent:
-        if parent.winfo_name() == 'left_parent':
-            parent.rowconfigure(LP_ROWS, weight=1)
-            parent.columnconfigure(LP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[0])
-        elif parent.winfo_name() == 'center_parent':
-            parent.rowconfigure(CP_ROWS, weight=1)
-            parent.columnconfigure(CP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[1])
-        else:
-            parent.rowconfigure(RP_ROWS, weight=1)
-            parent.columnconfigure(RP_COLS, weight=1)
-            parent.grid(sticky=tk.NSEW, row=APP_ROWS, column=APP_COLS[2])
-
-    APP.mainloop()
+from itertools import starmap
 
 
-def grid_widgets(widget: Type[GenericWidget], widget_opts: Mapping[str, Type[Sequence]]) -> None:
-    widget.grid_configure(**widget_opts)
+class LayoutManager():
+    def __init__(self):
+        self.APP = tk.Tk()
+        self.APP.title('Hangman In Python')
+        self.APP.rowconfigure(0, weight=1, pad=15, minsize=400)
+        self.APP.columnconfigure([0, 1], weight=1, pad=15, minsize=400)
 
-
-def initialize_frame(names: List[str], master: Optional[tk.Tk] = None) -> Tuple[ttk.Frame]:
-    """Take names and master ordering as left-center-right),
-    and return a tuple of Frame widgets"""
-
-    try:
+        self.LEFT = ttk.Frame(self.APP, name='lFrame')
+        self.RIGHT = ttk.Frame(self.APP, name='rFrame')
         # Left Frame containing the canvas widget
-        left = ttk.Frame(master, class_='ParentFrame', height=600, width=500, padding=15,
-                         relief=tk.GROOVE, name=names[0], borderwidth=1)
-        # Center Frame containing the Text widget to display previous choices
-        center = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
-                           name=names[1], borderwidth=1)
+        left = {'height': 600, 'width': 500, 'padding': 15,
+                'relief': tk.SUNKEN, 'borderwidth': 1}
         # Right Frame containing the canvas widget
-        right = ttk.Frame(master, class_='ParentFrame', width=500, padding=15,
-                          relief=tk.SUNKEN, name=names[2], borderwidth=1)
-    except IndexError:
-        print('3 names needed for frames')
-        return tuple()
+        right = {'height': 600, 'width': 500, 'padding': 15,
+                 'relief': tk.GROOVE, 'borderwidth': 1}
 
-    return (left, center, right)
+        self.initialize_frame(self.LEFT, left)
+        self.initialize_frame(self.RIGHT, right)
+
+    def set_frame_grids(self, parent: Tp.Type[ttk.Frame]) -> None:
+        # Left and Center Frames consist of 1x1 row, col
+        # Right Frames contains input, buttons, text; it consists of 2x1 row, col
+        cnfg = ('weight', 1), ('minsize', 400), ('pad', 15)
+        if parent.winfo_name() == 'LFrame':
+            parent.rowconfigure(0, dict(cnfg))
+            parent.columnconfigure(0, dict(cnfg))
+            parent.grid(sticky=tk.NSEW, row=0, column=0, padx=15, pady=15)
+        else:
+            parent.rowconfigure([0, 1], dict(cnfg))
+            parent.columnconfigure(0, dict(cnfg))
+            parent.grid(sticky=tk.NSEW, row=0, column=1, padx=15, pady=15)
+
+    def initialize_frame(self, frame: Tp.Type[ttk.Frame], frame_opts: Tp.Mapping[str, Tp.Any]) -> None:
+        """Configure Frame widgets configured with frame_opts"""
+        for opt in frame_opts:
+            frame[opt] = frame_opts[opt]
+        self.set_frame_grids(frame)
+
+    def start_app(self):
+        self.APP.mainloop()
 
 
 if __name__ == '__main__':
-    startup_app()
+    lm = LayoutManager()
+    lm.start_app()
