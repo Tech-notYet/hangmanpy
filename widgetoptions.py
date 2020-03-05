611673fd467061d6a561183218be540754f4a893
Tech-notYet <30681614+Tech-notYet@users.noreply.github.com>
2020-03-05 13:14:48 -0800

    more WidgetOptions fixes

--- widgetoptions.py
+++ widgetoptions.py
@@ -1,47 +1,47 @@
 import sys
-import typing as Tp
+import typing as tp
 from collections import defaultdict, namedtuple
 from tkinter import ttk
 from tkinter.ttk import tkinter as tk
 from pprint import pprint as pp
 
+DDict = tp.DefaultDict[str, tp.Sequence[str]]
+
 
 class WidgetOptions():
-    _WIDGETS = ('Button', 'Canvas', 'Checkbutton', 'Combobox', 'Entry', 'Frame', 'Label', 'LabelFrame', 'LabeledScale', 'Labelframe',
-               'Menubutton', 'Notebook', 'OptionMenu', 'PanedWindow', 'Panedwindow', 'Progressbar', 'Radiobutton',
-               'Scale', 'Scrollbar', 'Separator', 'Sizegrip', 'Spinbox', 'Toplevel', 'Treeview')
+    WIDGETS: tp.Tuple[str, ...] = ('Button', 'Canvas', 'Checkbutton', 'Combobox', 'Entry', 'Frame', 'Label', 'LabelFrame', 'LabeledScale', 'Labelframe',
+                                   'Menubutton', 'Notebook', 'OptionMenu', 'PanedWindow', 'Panedwindow', 'Progressbar', 'Radiobutton',
+                                   'Scale', 'Scrollbar', 'Separator', 'Sizegrip', 'Spinbox', 'Toplevel', 'Treeview')
 
-    def __new__(cls):
-        cls.__setattr__('OPTIONS', defaultdict(list))
-        pass
+    def __init__(self):
+        super().__init__()
+        self.OPTIONS: DDict = defaultdict(list)
 
-    # def __init__(self):
-    #     super().__init__()
-    #     self.WIDGETS = {w: w for w in self._WIDGETS}
-
-    def return_widget_options(self, widgetname: str) -> Tp.Mapping[str, Tp.Sequence[str]]:
-        OPTIONS = self.OPTIONS
+    @classmethod
+    def widget_options(cls, widgetname: tp.Optional[str] = None) -> DDict:
+        OPTIONS = defaultdict(list)
         wgname = widgetname
         if wgname is None:
             wgname = 'Frame'
-        if widgetname in self.WIDGETS:
+        if wgname in cls.WIDGETS:
             source = ''
             try:
-                if widgetname == 'Canvas' or widgetname == 'Toplevel':
-                    source = 'tk.'+widgetname+'().keys()'
-                elif widgetname == 'OptionMenu':
-                    source = 'ttk.'+widgetname+'(None, None).keys()'
+                if wgname in ('Canvas', 'Toplevel'):
+                    source = 'tk.' + wgname + '().keys()'
+                elif wgname == 'OptionMenu':
+                    source = 'ttk.' + wgname + '(None, None).keys()'
                 else:
-                    source = 'ttk.'+widgetname+'().keys()'
+                    source = 'ttk.'+wgname+'().keys()'
                 obj = eval(compile(source, '<string>', 'eval'))
-                OPTIONS[widgetname].extend(obj)
+                OPTIONS[wgname].extend(obj)
             except Exception as e:
-                print(f'{widgetname}\t{e.args}')
-        return OPTIONS
+                print(f'{wgname}\t{e.args}')
+        return OPTIONS[wgname]
+
 
 if __name__ == '__main__':
-    wo = WidgetOptions()
-    wg = wo.WIDGETS[sys.argv[1]]
-    wopts = wo.return_widget_options(wg)
-    canvas = namedtuple(wg, wopts[wg])
-    print(canvas)
+    pp(WidgetOptions.widget_options(sys.argv[1]))
+    # wg = wo.WIDGETS[sys.argv[1]]
+    # wopts = wo._widget_options(wg)
+    # canvas = namedtuple(wg, wopts[wg])
+    # print(wo)
